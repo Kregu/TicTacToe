f5a55df462e9f681a9070ef08ec4d2b3a742a615
Sergey Gorokhov <sergey.login@gmail.com>
2022-07-30 12:42:43 +0300

    add: view interface

---
+++ src/main/java/com/github/kregu/tictactoe/view/View.java
@@ -0,0 +1,11 @@
+package com.github.kregu.tictactoe.view;
+
+import com.github.kregu.tictactoe.model.Board;
+
+public interface View {
+
+    void showBoard (Board board);
+
+    void showGameName ();
+
+}
